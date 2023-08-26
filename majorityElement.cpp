95be2ce9b3076cbba6a180434c6adeaa6eafa4f5
pctien <pctien>
2023-08-26 23:53:03 +0700

    first commit

---
+++ majorityElement.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        /*
+            Moore Voting Algorithm 
+        */
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+};
