6cf1013a6b4dc7e3c01c5d8dfbb5a74389d7c90c
pctien <pctien>
2023-08-30 00:30:04 +0700

    find peak element by BNS

---
+++ romanToInt.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int intVal[]={1,5,10,50,100,500,1000};
+        char romanVal[]={'I','V','X','L','C','D','M'};
+            
+    }
+};
